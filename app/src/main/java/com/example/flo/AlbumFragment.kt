8a92bb4e5be3ab5293c47f919aff8d03b5172284
박민준 <alswns6541@gmail.com>
2022-04-01 17:11:14 +0900

    섹션3 완료

--- app/src/main/java/com/example/flo/AlbumFragment.kt
+++ app/src/main/java/com/example/flo/AlbumFragment.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import com.example.flo.databinding.FragmentAlbumBinding
 
@@ -20,6 +21,11 @@ class AlbumFragment : Fragment() {
         binding.albumBackIv.setOnClickListener {
             (context as MainActivity).supportFragmentManager.beginTransaction().replace(R.id.main_frm,HomeFragment()).commitAllowingStateLoss()
         }
+
+        binding.songLalacLayout.setOnClickListener {
+            Toast.makeText(activity,"LILAC",Toast.LENGTH_SHORT).show()
+        }
+
         return binding.root
     }
 }
